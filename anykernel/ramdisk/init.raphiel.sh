bb1dbc6f2784fa602c4a1b3c9edbd6df4d915983
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-06-07 20:46:38 +0700

    spectrum, init: fixups
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- anykernel/ramdisk/init.raphiel.sh
+++ anykernel/ramdisk/init.raphiel.sh
@@ -21,20 +21,21 @@ function get-set-forall() {
 
 {
 
-# devfreq
-get-set-forall /sys/class/devfreq/qcom,cpubw*/governor bw_hwmon
-restorecon -R /sys/class/devfreq/qcom,cpubw*
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/sample_ms 4
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/io_percent 34
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/hist_memory 20
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/hyst_length 10
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/low_power_ceil_mbps 0
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/low_power_io_percent 34
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/low_power_delay 20
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/guard_band_mbps 0
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/up_scale 250
-get-set-forall /sys/class/devfreq/qcom,cpubw*/bw_hwmon/idle_mbps 1600
-get-set-forall /sys/class/devfreq/qcom,mincpubw*/governor cpufreq
+# devfreq tuning
+# Qualcomm is using obscure directory for devfreq, so follow up
+restorecon -R /sys/class/devfreq/*qcom,cpubw*
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/governor bw_hwmon
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/sample_ms 4
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/io_percent 34
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/hist_memory 20
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/hyst_length 10
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_ceil_mbps 0
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_io_percent 34
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/low_power_delay 20
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/guard_band_mbps 0
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/up_scale 250
+get-set-forall /sys/class/devfreq/*qcom,cpubw*/bw_hwmon/idle_mbps 1600
+get-set-forall /sys/class/devfreq/*qcom,mincpubw*/governor cpufreq
 
 # ZRAM
 write /sys/block/zram0/reset 1
